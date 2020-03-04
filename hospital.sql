a02364dee7296239ec1a4cbf10f71324c12a70f0
Fernanch-03 <fefemachado03@gmail.com>
2020-03-04 10:57:50 -0300

    b

---
+++ hospital.sql
@@ -0,0 +1,62 @@
+create database hospital;
+
+use hospital;
+
+ create table hospital(
+ id int auto_increment primary key,
+ nome varchar(45) not null,
+ endereco varchar(100) not null
+
+);
+
+ create table pacientes(
+ id int auto_increment primary key,
+ hospital int,
+ nome varchar(50) not null,
+ cpf char(14) not null,
+ rg char(20) not null,
+ idade int(3) not null,
+ senha varchar(45) not null,
+ foreign key (hospital) references hospital(id)
+ 
+ );
+ 
+ create table funcionarios(
+ id int auto_increment primary key,
+ hospital int,
+ nome varchar(50) not null,
+ cpf char(14) not null,
+ rg char(20) not null,
+ idade int(3) not null,
+ senha varchar(45) not null,
+ vlHora decimal(5,2) not null,
+ foreign key (hospital) references hospital(id)
+ 
+ );
+ 
+ create table prontosocorro(
+ id int auto_increment primary key,
+ hospital int,
+ endereco varchar(100) not null,
+ foreign key (hospital) references hospital(id)
+ 
+ );
+ 
+ create table medicos(
+ id int auto_increment primary key,
+ funcionario int,
+ crm varchar(45) not null,
+ especialidade varchar(45),
+ foreign key (funcionario) references funcionario(id)
+ 
+ );
+ 
+ create table enfermeiros(
+ id int auto_increment primary key,
+ funcionario int,
+ corem varchar(45) not null,
+ foreign key (funcionario) references funcionario(id)
+ 
+ );
+ 
+ 
