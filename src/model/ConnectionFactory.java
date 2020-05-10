6a336fdbfde9c782a67e3e527b0a52d844f0564b
Giulliano Rossi <giulliano.scatalon.rossi@gmail.com>
2020-05-10 15:38:27 -0300

    Bug com a conexão do MySQL corrigido

--- src/model/ConnectionFactory.java
+++ src/model/ConnectionFactory.java
@@ -7,13 +7,13 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
     
-        public Connection getConnection(){
+        public Connection getConnection() throws RuntimeException{
             try{
+                Class.forName("com.mysql.jdbc.Driver");  // <Giulliano> Verifique se o driver esta junto do classpath
                 return DriverManager.getConnection("jdbc:mysql://localhost:3307/hospital","root","usbw");
             }
             catch(SQLException e){
-                throw new RuntimeException(e);
-            
+                throw new RuntimeException(e.getMessage());
             }
         
         }
